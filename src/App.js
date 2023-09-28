c1ae43dd96033a5cabfdaf72f21dc7d97256ab92
Paul Yi <pauljsyi@gmail.com>
2023-09-28 12:37:08 -0600

    modal practice

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import React, { useState, useEffect } from "react";
+import Modal from "./Modal";
 
+const BUTTON_WRAPPER_STYLES = {
+  position: "relative",
+  zIndex: 1,
+};
+
+const OTHER_CONTENT_STYLES = {
+  position: "relative",
+  zIndex: 2,
+  backgroundColor: "red",
+  padding: "10px",
+};
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="App">
-
+      <div style={BUTTON_WRAPPER_STYLES}>
+        <button onClick={() => setIsOpen(!isOpen)}>Open Modal</button>
+        <Modal open={isOpen}>Fancy MODAL</Modal>
+      </div>
+      <div style={OTHER_CONTENT_STYLES}>Other Content</div>
     </div>
   );
 }
